448b89b4239d57ef7a544824e5c09342d50a61fc
guozijing1234 <34123092+guozijing1234@users.noreply.github.com>
2026-06-21 00:17:13 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cat-tracker-v4';
+const CACHE = 'cat-tracker-v6';
 const ASSETS = ['./index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
